fd3fb003c71532d310b3171c9b6235ea898cba9f
Vladislav Zurba <vladislav.zurba@manchester.ac.uk>
2026-06-08 11:43:59 +0100

    Removed weno7 and linear_upwind5 reconstruction schemes

--- packed_bed/axial_schemes.py
+++ packed_bed/axial_schemes.py
@@ -18,11 +18,9 @@ SUPPORTED_SCHEMES = (
     "upwind1",
     "central",
     "linear_upwind2",
-    "linear_upwind5",
     "muscl_minmod",
     "weno3",
     "weno5",
-    "weno7",
 )
 
 
@@ -75,69 +73,6 @@ def _weno5_value(v0, v1, v2, v3, v4, small_eps):
     return (alpha0 * q0 + alpha1 * q1 + alpha2 * q2) / alpha_sum
 
 
-def _weno7_value(v0, v1, v2, v3, v4, v5, v6, small_eps):
-    q0 = -0.25 * v0 + (13.0 / 12.0) * v1 - (23.0 / 12.0) * v2 + (25.0 / 12.0) * v3
-    q1 = (1.0 / 12.0) * v1 - (5.0 / 12.0) * v2 + (13.0 / 12.0) * v3 + 0.25 * v4
-    q2 = -(1.0 / 12.0) * v2 + (7.0 / 12.0) * v3 + (7.0 / 12.0) * v4 - (1.0 / 12.0) * v5
-    q3 = 0.25 * v3 + (13.0 / 12.0) * v4 - (5.0 / 12.0) * v5 + (1.0 / 12.0) * v6
-
-    beta0 = (
-        547.0 * v0**2
-        - 3882.0 * v0 * v1
-        + 4642.0 * v0 * v2
-        - 1854.0 * v0 * v3
-        + 7043.0 * v1**2
-        - 17246.0 * v1 * v2
-        + 7042.0 * v1 * v3
-        + 11003.0 * v2**2
-        - 9402.0 * v2 * v3
-        + 2107.0 * v3**2
-    ) / 240.0
-    beta1 = (
-        267.0 * v1**2
-        - 1642.0 * v1 * v2
-        + 1602.0 * v1 * v3
-        - 494.0 * v1 * v4
-        + 2843.0 * v2**2
-        - 5966.0 * v2 * v3
-        + 1922.0 * v2 * v4
-        + 3443.0 * v3**2
-        - 2522.0 * v3 * v4
-        + 547.0 * v4**2
-    ) / 240.0
-    beta2 = (
-        547.0 * v2**2
-        - 2522.0 * v2 * v3
-        + 1922.0 * v2 * v4
-        - 494.0 * v2 * v5
-        + 3443.0 * v3**2
-        - 5966.0 * v3 * v4
-        + 1602.0 * v3 * v5
-        + 2843.0 * v4**2
-        - 1642.0 * v4 * v5
-        + 267.0 * v5**2
-    ) / 240.0
-    beta3 = (
-        2107.0 * v3**2
-        - 9402.0 * v3 * v4
-        + 7042.0 * v3 * v5
-        - 1854.0 * v3 * v6
-        + 11003.0 * v4**2
-        - 17246.0 * v4 * v5
-        + 4642.0 * v4 * v6
-        + 7043.0 * v5**2
-        - 3882.0 * v5 * v6
-        + 547.0 * v6**2
-    ) / 240.0
-
-    alpha0 = (1.0 / 35.0) / (beta0 + small_eps**2) ** 2
-    alpha1 = (12.0 / 35.0) / (beta1 + small_eps**2) ** 2
-    alpha2 = (18.0 / 35.0) / (beta2 + small_eps**2) ** 2
-    alpha3 = (4.0 / 35.0) / (beta3 + small_eps**2) ** 2
-    alpha_sum = alpha0 + alpha1 + alpha2 + alpha3
-    return (alpha0 * q0 + alpha1 * q1 + alpha2 * q2 + alpha3 * q3) / alpha_sum
-
-
 def _linear_upwind5_value(v0, v1, v2, v3, v4):
     return (2.0 * v0 - 13.0 * v1 + 47.0 * v2 + 27.0 * v3 - 3.0 * v4) / 60.0
 
@@ -165,13 +100,6 @@ def reconstruct_face_left_value(cell_accessor, face_index, n_cells, scheme_name,
     if scheme_name == "linear_upwind2":
         return 1.5 * phi_L - 0.5 * phi_LL
 
-    if scheme_name == "linear_upwind5":
-        if face_index < 3 or (n_cells is not None and face_index > n_cells - 2):
-            return 1.5 * phi_L - 0.5 * phi_LL
-        phi_LLL = cell_accessor(idx_cell_L - 2)
-        phi_RR = cell_accessor(idx_cell_R + 1)
-        return _linear_upwind5_value(phi_LLL, phi_LL, phi_L, phi_R, phi_RR)
-
     if scheme_name == "muscl_minmod":
         zero_like = 0.0 * small_eps
         delta_up = phi_L - phi_LL
@@ -188,15 +116,6 @@ def reconstruct_face_left_value(cell_accessor, face_index, n_cells, scheme_name,
         phi_LLL = cell_accessor(idx_cell_L - 2)
         phi_RR = cell_accessor(idx_cell_R + 1)
         return _weno5_value(phi_LLL, phi_LL, phi_L, phi_R, phi_RR, small_eps)
-
-    if scheme_name == "weno7":
-        if face_index < 4 or (n_cells is not None and face_index > n_cells - 3):
-            return reconstruct_face_left_value(cell_accessor, face_index, "weno5", small_eps, n_cells=n_cells)
-        phi_LLLL = cell_accessor(idx_cell_L - 3)
-        phi_LLL = cell_accessor(idx_cell_L - 2)
-        phi_RR = cell_accessor(idx_cell_R + 1)
-        phi_RRR = cell_accessor(idx_cell_R + 2)
-        return _weno7_value(phi_LLLL, phi_LLL, phi_LL, phi_L, phi_R, phi_RR, phi_RRR, small_eps)
 
     raise RuntimeError(f"Unsupported scheme '{scheme_name}' passed reconstruction.")
 
@@ -224,13 +143,6 @@ def reconstruct_face_right_value(cell_accessor, face_index, n_cells, scheme_name
     if scheme_name == "linear_upwind2":
         return 1.5 * phi_R - 0.5 * phi_RR
 
-    if scheme_name == "linear_upwind5":
-        if face_index < 2 or face_index > n_cells - 3:
-            return 1.5 * phi_R - 0.5 * phi_RR
-        phi_RRR = cell_accessor(idx_cell_R + 2)
-        phi_LL = cell_accessor(idx_cell_L - 1)
-        return _linear_upwind5_value(phi_RRR, phi_RR, phi_R, phi_L, phi_LL)
-
     if scheme_name == "muscl_minmod":
         zero_like = 0.0 * small_eps
         delta_up = phi_RR - phi_R
@@ -247,15 +159,6 @@ def reconstruct_face_right_value(cell_accessor, face_index, n_cells, scheme_name
         phi_RRR = cell_accessor(idx_cell_R + 2)
         phi_LL = cell_accessor(idx_cell_L - 1)
         return _weno5_value(phi_RRR, phi_RR, phi_R, phi_L, phi_LL, small_eps)
-
-    if scheme_name == "weno7":
-        if face_index < 3 or face_index > n_cells - 4:
-            return reconstruct_face_right_value(cell_accessor, face_index, n_cells, "weno5", small_eps)
-        phi_RRR = cell_accessor(idx_cell_R + 2)
-        phi_RRRR = cell_accessor(idx_cell_R + 3)
-        phi_LL = cell_accessor(idx_cell_L - 1)
-        phi_LLL = cell_accessor(idx_cell_L - 2)
-        return _weno7_value(phi_RRRR, phi_RRR, phi_RR, phi_R, phi_L, phi_LL, phi_LLL, small_eps)
 
     raise RuntimeError(f"Unsupported scheme '{scheme_name}' passed reconstruction.")
 
